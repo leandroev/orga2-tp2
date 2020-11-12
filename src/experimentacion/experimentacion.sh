67c4897b6700526c63f7d5f335177a45546ee497
Leandro Vigali <lvigali@dc.uba.ar>
2020-11-12 18:24:46 -0300

    fix exp.sh

--- src/experimentacion/experimentacion.sh
+++ src/experimentacion/experimentacion.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
-file_location1=resultados/print_tp2.cvs
-file_location2=resultados/print_tp2_exp.cvs
-file_location3=resultados/_ciclos.cvs
-file_location4=resultados/_sec.cvs
-file_location5=resultados/_microsec.cvs
+file_location1=resultados/$1_print_tp2.cvs
+file_location2=resultados/$1_print_tp2_exp.cvs
+file_location3=resultados/$1_ciclos.cvs
+file_location4=resultados/$1_sec.cvs
+file_location5=resultados/$1_microsec.cvs
 
 if [ -e  $file_location1 ]; then
 	rm $file_location1
@@ -15,33 +15,44 @@ if [  ! -d  resultados ]; then
 	mkdir resultados
 fi
 
-if [ $# -lt 2 ];	then
-	echo "Faltan argumentos: tipo de implementación asm o c y una imagen. Opcionalmente ingresar de forma ordenada cantidad de iteraciones, un offsetx y un offsety"
+if [ $# -lt 3 ];	then
+	echo "Faltan argumentos: 
+	Parametro 1: Sugerencias - Si el filtro es ImagenFantasma ingresar ''
+							   Si es ImagenFantasma_enteros escribir 'enteros'
+							   Si es ImagenFantasma_x4 escribir: 'x4'
+	
+	Parametro 2: El tipo de implementación asm o c
+	Parametro 3: Directorio de la imagen. 
+	
+	Parametros opcionales:
+		Parametro 4: cantidad de iteraciones
+		Parametro 5: offsetx
+		Parametro 6: offsety"
 else
+	if [ $# -eq 3 ];	then
 	echo "Cantidad de iteraciones 1!"
-	if [ $# -eq 2 ];	then
-		../build/tp2 ImagenFantasma -i $1 $2 0 0 >> $file_location1
-		./tp2_exp ImagenFantasma -i $1 $2 0 0 >> $file_location2
+		../build/tp2 ImagenFantasma -i $2 $3 0 0 >> $file_location1
+		./tp2_exp ImagenFantasma -i $2 $3 0 0 >> $file_location2
 	else
-		echo "Cantidad de iteraciones $3!"
-		if [ $# -eq 3 ];	then
-			for (( i = 0; i < $3; i++ )); do
-				../build/tp2 ImagenFantasma -i $1 $2 0 0 >> $file_location1
-				./tp2_exp ImagenFantasma -i $1 $2 0 0 >> $file_location2
-			done
-		fi	
-
+		echo "Cantidad de iteraciones $4!"
 		if [ $# -eq 4 ];	then
-			for (( i = 0; i < $3; i++ )); do
-				../build/tp2 ImagenFantasma -i $1 $2 $4 0 >> $file_location1
-				./tp2_exp ImagenFantasma -i $1 $2 $4 0 >> $file_location2
+			for (( i = 0; i < $4; i++ )); do
+				../build/tp2 ImagenFantasma -i $2 $3 0 0 >> $file_location1
+				./tp2_exp ImagenFantasma -i $2 $3 0 0 >> $file_location2
 			done
 		fi	
 
-		if [ $# -gt 4 ]; then
-			for (( i = 0; i < $3; i++ )); do
-				../build/tp2 ImagenFantasma -i $1 $2 $4 $5 >> $file_location1
-				./tp2_exp ImagenFantasma -i $1 $2 $4 $5 >> $file_location2
+		if [ $# -eq 5 ];	then
+			for (( i = 0; i < $4; i++ )); do
+				../build/tp2 ImagenFantasma -i $2 $3 $5 0 >> $file_location1
+				./tp2_exp ImagenFantasma -i $2 $3 $5 0 >> $file_location2
+			done
+		fi	
+
+		if [ $# -gt 5 ]; then
+			for (( i = 0; i < $4; i++ )); do
+				../build/tp2 ImagenFantasma -i $2 $3 $5 $6 >> $file_location1
+				./tp2_exp ImagenFantasma -i $2 $3 $5 $6 >> $file_location2
 			done
 		fi
 	fi
